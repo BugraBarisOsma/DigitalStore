ac03e365a69d0174187b97487c6155342e087fd2
BugraBarisOsma <bugrabarisosma@gmail.com>
2024-08-11 02:20:14 +0300

    last migration

---
+++ DigitalStore.Data/Migrations/20240810151830_HangfireAndBase.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DigitalStore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class HangfireAndBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "68302ddf-5c72-4fbd-8995-f2368bb78c8e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "33fbf6de-b4c7-4661-983b-e271bfa0475a", "b5d9557a-ab14-471f-88ed-6238c09dd75e" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "33fbf6de-b4c7-4661-983b-e271bfa0475a");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "b5d9557a-ab14-471f-88ed-6238c09dd75e");
+
+            migrationBuilder.RenameColumn(
+                name: "IsUsed",
+                table: "Coupons",
+                newName: "IsActive");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Products",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Products",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Orders",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Orders",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "OrderDetails",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "OrderDetails",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "OrderDetails",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Coupons",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Coupons",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "CategoryProducts",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                table: "CategoryProducts",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "CategoryProducts",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "CategoryProducts",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Categories",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Categories",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Categories",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "029c0ed6-dd79-4c54-9c87-bfc651c009df", null, "Admin", "ADMIN" },
+                    { "4902ed63-8360-42fb-9e3f-e64fa7306c58", null, "Customer", "CUSTOMER" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "ConcurrencyStamp", "NormalizedEmail", "NormalizedUserName", "Password", "Points", "Role", "SecurityStamp", "Surname", "Username", "WalletBalance", "isActive" },
+                values: new object[] { "b756e11a-1cc1-4b20-82ca-b839bdb40d9c", "1f63a2ff-4ec8-49f5-8b63-f32e2331dd32", "ADMIN@EXAMPLE.COM", "ADMIN", "admin123", 10, "Admin", "e69c7fe5-4338-495c-8125-8f966ad57bd8", "admin", "admin", 2000m, true });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "029c0ed6-dd79-4c54-9c87-bfc651c009df", "b756e11a-1cc1-4b20-82ca-b839bdb40d9c" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4902ed63-8360-42fb-9e3f-e64fa7306c58");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "029c0ed6-dd79-4c54-9c87-bfc651c009df", "b756e11a-1cc1-4b20-82ca-b839bdb40d9c" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "029c0ed6-dd79-4c54-9c87-bfc651c009df");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "b756e11a-1cc1-4b20-82ca-b839bdb40d9c");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "OrderDetails");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "OrderDetails");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "OrderDetails");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Coupons");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Coupons");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "CategoryProducts");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "CategoryProducts");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "CategoryProducts");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "CategoryProducts");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Categories");
+
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "Coupons",
+                newName: "IsUsed");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "33fbf6de-b4c7-4661-983b-e271bfa0475a", null, "Admin", "ADMIN" },
+                    { "68302ddf-5c72-4fbd-8995-f2368bb78c8e", null, "Customer", "CUSTOMER" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "ConcurrencyStamp", "NormalizedEmail", "NormalizedUserName", "Password", "Points", "Role", "SecurityStamp", "Surname", "Username", "WalletBalance", "isActive" },
+                values: new object[] { "b5d9557a-ab14-471f-88ed-6238c09dd75e", "904dfed9-8890-4216-8db4-969f32b0c219", "ADMIN@EXAMPLE.COM", "ADMIN", "admin123", 0, "Admin", "f193d420-fa68-4fcb-8f91-8568bcd6ca7a", "admin", "admin", 0m, true });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "33fbf6de-b4c7-4661-983b-e271bfa0475a", "b5d9557a-ab14-471f-88ed-6238c09dd75e" });
+        }
+    }
+}
